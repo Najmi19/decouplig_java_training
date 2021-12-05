2caef94f38e316c4445a89e9eedb6421b5b62b9f
Najmi Mehdi <najmimehdi70@gmail.com>
2021-12-05 12:57:22 +0100

    part3

---
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -0,0 +1,30 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+import java.security.SecureRandom;
+//import fr.lernejo.logger.Logger;
+public class Launcher {
+    public static Logger logger = LoggerFactory.getLogger("mylauncher");
+    public static void main(String[] args){
+        // Player player= new Player();
+        //Simulation simulation = new Simulation(player);
+
+        switch (args[0]) {
+            // avec humain
+            case "-interactive" : {
+                Player player = new HumanPlayer();
+                Simulation start_simulation = new Simulation(player);
+                SecureRandom random = new SecureRandom();
+
+                // long randomNumber = random.nextLong(); // génère un nombre entre Long.MIN_VALUE et Long.MAX_VALUE
+                long randomNumber = random.nextLong(100); // génère un nombre entre 0 (inclus) et 100 (exclus)
+                logger.log("Avec humain");
+                start_simulation.initialize(randomNumber,Long.MAX_VALUE);
+                start_simulation.loopUntilPlayerSucceed();
+                break;
+            }
+        }
+    }
+}
+
